b853a3730deefb330993a3c59fb88f9badd3854d
ajile <v.v.milkov@gmail.com>
2014-11-26 12:54:39 +0300

    Added fixedZIndex flag

--- app/utils/highest-zindex.js
+++ app/utils/highest-zindex.js
@@ -1,9 +1,10 @@
 export default function () {
     var $ = Ember.$;
     var max = Math.max.apply(null, $.map($(':visible:not(.highest)'), function(element){
-        if($(element).css('position') === 'absolute' || $(element).css('position') === 'relative')
-            return parseInt($(element).css('z-index')) || 1;  // jshint ignore: line
-        })
-    );
+        if($(element).css('position') === 'absolute' || $(element).css('position') === 'relative'){
+            return ($(element).css('z-index') >> 0) || 1;  // jshint ignore: line
+        }
+        return 0;
+    }));
     return max;
 }
