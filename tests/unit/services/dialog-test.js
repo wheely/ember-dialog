f2de08a9f42e77f4c713258c01e6fb3e238166cf
Waleed <qadiwaleed@gmail.com>
2016-11-27 19:28:43 +0200

    added new tests to check for dublicate opened dialogs, and to test the newly added method destroyAllDiaglos

--- tests/unit/services/dialog-test.js
+++ tests/unit/services/dialog-test.js
@@ -12,6 +12,41 @@ test('it exists', function(assert) {
   let service = this.subject();
   Ember.run(() => {
     service.show(hbs`<div class="dialog-layout">layout</div>`, hbs`template`);
-    assert.equal($(".dialog-layout").size(), 1);
+    assert.equal($(".dialog-layout").size(), 1, "Opened dialog");  
+  });
+});
+
+test('it exists once', function(assert) {
+  let service = this.subject();
+  Ember.run(() => {
+    service.show(hbs`<div class="dialog-layout">layout</div>`, hbs`template`, {}, {id: "1"});
+    service.show(hbs`<div class="dialog-layout">layout</div>`, hbs`template`, {}, {id: "1"});
+    assert.equal($(".dialog-layout").size(), 2, "Dialog with id 1 opened once");
+  });
+});
+
+test('it exists twice without setting id', function(assert) {
+  let service = this.subject();
+  Ember.run(() => {
+    service.show(hbs`<div class="dialog-layout">layout</div>`, hbs`template`);
+    service.show(hbs`<div class="dialog-layout">layout</div>`, hbs`template`);
+    assert.equal($(".dialog-layout").size(), 4, "New Dialog with no id opened twice");
+  });
+});
+
+test('it exists twice with two different ids', function(assert) {
+  let service = this.subject();
+  Ember.run(() => {
+    service.show(hbs`<div class="dialog-layout">layout</div>`, hbs`template`, {}, {id: "2"});
+    service.show(hbs`<div class="dialog-layout">layout</div>`, hbs`template`, {}, {id: "3"});
+    assert.equal($(".dialog-layout").size(), 6, "New Dialog with different ids (1, 2) opened twice");
+  });
+});
+
+test('distory all opened dialogs', function(assert) {
+  let service = this.subject();
+  Ember.run(() => {
+    service.destroyAllPresenter();
+    assert.equal($(".dialog-layout").size(), 0, "All dialogs have been destroyed");
   });
 });
