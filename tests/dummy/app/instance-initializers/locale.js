24a41e003ba65526d8c9f6ca5a5afc1fc3018bae
ajile <v.v.milkov@gmail.com>
2016-11-28 09:59:05 +0000

    Updated and cleaned up dependencies

--- tests/dummy/app/instance-initializers/locale.js
+++ tests/dummy/app/instance-initializers/locale.js
@@ -2,29 +2,28 @@ import Ember from 'ember';
 import LocaleService from '../services/locale';
 import changeLocaleHandler from '../utils/change-locale';
 
-function langHandler(container, locale) {
+function langHandler(appInstance, locale) {
   var languageCode = locale.get('languageCode');
-  container.lookup('service:i18n').set('locale', languageCode);
+  appInstance.lookup('service:i18n').set('locale', languageCode);
   changeLocaleHandler();
 }
 
-export function initialize(application) {
-  const registry = application.registry;
-  const container = application.container;
-  registry.register('service:locale', LocaleService, { singleton: true });
-  registry.injection('controller', 'locale', 'service:locale');
-  registry.injection('route', 'locale', 'service:locale');
-  registry.injection('component', 'locale', 'service:locale');
-  registry.injection('model', 'locale', 'service:locale');
+export function initialize(appInstance) {
+  const { application } = appInstance;
+  application.register('service:locale', LocaleService, { singleton: true });
+  application.inject('controller', 'locale', 'service:locale');
+  application.inject('route', 'locale', 'service:locale');
+  application.inject('component', 'locale', 'service:locale');
+  application.inject('model', 'locale', 'service:locale');
 
   // The global locale service (it's contain determined language)
-  const locale = container.lookup('service:locale');
+  const locale = appInstance.lookup('service:locale');
 
   // Set up application language
-  langHandler(container, locale);
+  langHandler(appInstance, locale);
 
   // On changing language, change it everywhere
-  locale.on('languageDidChange', this, Ember.run.bind(this, langHandler, container, locale));
+  locale.on('languageDidChange', this, Ember.run.bind(this, langHandler, appInstance, locale));
 }
 
 export default { name: 'locale', initialize };
