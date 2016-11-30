8a009d016751561e19b640075638146ab6e5a510
ajile <v.v.milkov@gmail.com>
2016-11-30 09:23:12 +0000

    Changed argument options to undefined
    Because default value (empty hash) doesn't set in the show method and it breaks when trying to get id from undefined variable.

--- tests/dummy/app/controllers/cookbook/animation.js
+++ tests/dummy/app/controllers/cookbook/animation.js
@@ -8,7 +8,7 @@ export default Ember.Controller.extend({
       this.get("dialog").alert(hbs`Dialog's body`, this);
     },
     showAnimatedDialog() {
-      this.get("dialog").alert(hbs`Dialog's body`, this, null, "presenter-animated");
+      this.get("dialog").alert(hbs`Dialog's body`, this, undefined, "presenter-animated");
     },
     showAnimation(animationToShow, animationToHide, delay=1000) {
       this.get("dialog").alert(hbs`Dialog's body`, this, {animationToShow, animationToHide, delay}, "presenter-animate-css");
